479093d58d21e91f631b84bab6d2c9c9bb0ca4e3
Arb-Luan <arbeit.hluan@gmail.com>
2024-06-25 11:25:06 -0300

    Primeiro Commit

---
+++ Descanso Garantido Hotel/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Hotel.h"
+using namespace std;
+void menu();
+int main()
+{
+    menu();
+    FILE*arquivo;
+    //arquivo=fopen("arquivo_saida.btxt",);
+
+
+}
+void menu()
+{
+    cout<<"Bem vindo ao hotel Descanso Garantido"<<endl;
+    cout<<"Diarias de 14h a 12h do dia seguinte
+
+}
